8edd789051504222924bf75e9955cc6f95604b0b
rlaqudwls25 <rlaqudwls157@gmail.com>
2021-04-19 14:39:32 +0900

    Set객체를 사용해서 삽입 순서대로 요소를 순회하는 풀이 추가

--- Programmers/3.js
+++ Programmers/3.js
@@ -2,6 +2,7 @@
 // 문제 설명
 // 정수 배열 numbers가 주어집니다. numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
 
+//첫 번째 풀이
 function solution(numbers) {
   let answer = [];
 
@@ -15,4 +16,17 @@ function solution(numbers) {
   answer.sort((a, b) => a - b);
 
   return answer;
+}
+
+//두 번째 풀이
+function solution(numbers) {
+  const answer = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      answer.push(numbers[i] + numbers[j]);
+    }
+  }
+  const result = [...new Set(answer)];
+  return result.sort((a, b) => a - b);
 }
