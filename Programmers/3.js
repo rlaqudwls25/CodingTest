027b06ee3338cd84d410f601a8a03a450b4f23f7
rlaqudwls25 <rlaqudwls157@gmail.com>
2021-04-19 23:08:09 +0900

    프로그래머스 1단계 풀이

--- Programmers/3.js
+++ Programmers/3.js
@@ -21,7 +21,6 @@ function solution(numbers) {
 //두 번째 풀이
 function solution(numbers) {
   const answer = [];
-
   for (let i = 0; i < numbers.length; i++) {
     for (let j = i + 1; j < numbers.length; j++) {
       answer.push(numbers[i] + numbers[j]);
