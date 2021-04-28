25b76425b5610687d2b62475fdc0d1875d7b6b89
rlaqudwls25 <rlaqudwls157@gmail.com>
2021-04-28 23:08:32 +0900

    prototype

---
+++ JS/proto.js
@@ -0,0 +1,13 @@
+// 생성자 함수 Person의 prototype에 getName이라는 메서드를 지정
+const Person = function (name) {
+  this._name = name;
+};
+
+Person.prototype.getName = function () {
+  return this._name;
+};
+
+const bj = new Person("Suzi");
+bj.__proto__.getName();
+
+Person.prototype === bj.__proto__;
