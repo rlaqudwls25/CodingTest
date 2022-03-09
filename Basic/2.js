a8d6c42b51780ea9dbd297ac1f86d636310b3ccb
rlaqudwls25 <rlaqudwls157@gmail.com>
2022-03-09 15:18:10 +0900

    문자 찾기

---
+++ Basic/2.js
@@ -0,0 +1,17 @@
+function solution(s) {
+  let count = 0;
+
+  for (let x of s) {
+    if (x === "R") {
+      count += 1;
+    }
+  }
+
+  return answer;
+}
+solution("COMPUTERPROGRAMMINGRR");
+
+function solution(answer, a) {
+  return answer.split("R").length - 1;
+}
+solution("COMPUTERPROGRAMMINGRR", "R");
