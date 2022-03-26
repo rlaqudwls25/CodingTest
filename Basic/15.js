d97e1c45736956144d820dbe44080f7831a0f204
rlaqudwls25 <rlaqudwls157@gmail.com>
2022-03-27 00:40:10 +0900

    회문 문자열 풀이방법2

--- Basic/15.js
+++ Basic/15.js
@@ -2,6 +2,7 @@ function solution(str) {
   // 회문을 검사할 때 대소문자를 구분하지 않기 때문에 toLowerCase를 사용해서 소문자로 바꿔준다.
   // 첫번째 마지막번째의 알파벳이 동일한지 Check
 
+  // 첫번째 풀이 방법
   let answer = "Yes";
   let lowStr = str.toLowerCase();
   let len = str.length;
@@ -11,6 +12,16 @@ function solution(str) {
       answer = "No";
     }
   }
+
+  // 두번째 풀이 방법
+  //   let answer = ""
+  //   s = str.toLowerCase();
+
+  //   if(s.split('').reverse().join('') === s) {
+  //     answer = "Yes"
+  //   } else {
+  //     answer = "No"
+  //   }
 
   return answer;
 }
