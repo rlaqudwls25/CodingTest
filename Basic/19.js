417d8a788c72ad5a4666600fd586b854ae535aae
rlaqudwls25 <rlaqudwls157@gmail.com>
2022-04-01 23:56:19 +0900

    문자열 압축

---
+++ Basic/19.js
@@ -0,0 +1,12 @@
+function solution(s) {
+  let answer = "";
+  let count = 0;
+  let s1 = s + " ";
+  console.log(s1);
+
+  for (let i = 0; i < s.length; i++) {}
+
+  return answer;
+}
+s = "KKHSSSSSSSE";
+solution(s);
