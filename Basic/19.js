c9ac5ebdf9e45ca05bff669e89766c10c3928543
rlaqudwls25 <rlaqudwls157@gmail.com>
2022-04-02 00:13:57 +0900

    문자열 압축 풀이

--- Basic/19.js
+++ Basic/19.js
@@ -1,12 +1,23 @@
-function solution(s) {
-  let answer = "";
-  let count = 0;
-  let s1 = s + " ";
-  console.log(s1);
+function solution(str) {
+  // 앞에서부터 하나하나씩 비교
+  // 마지막 문자는 비교할 부분이 없으니 s=str+""를 만들어준다.
 
-  for (let i = 0; i < s.length; i++) {}
+  let answer = "";
+  let count = 1;
+  s = str + " ";
+  for (let i = 0; i < s.length - 1; i++) {
+    if (s[i] === s[i + 1]) {
+      count++;
+    } else {
+      answer += s[i];
+      if (count > 1) {
+        answer += String(count);
+        count = 1;
+      }
+    }
+  }
 
   return answer;
 }
-s = "KKHSSSSSSSE";
-solution(s);
+str = "KKHSSSSSSSE";
+solution(str);
