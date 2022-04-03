886a64094b8f2484dee2be8bd12e028995317eb3
rlaqudwls25 <rlaqudwls157@gmail.com>
2022-04-03 23:15:51 +0900

    자리수의 합 풀이

---
+++ Basic/20.js
@@ -0,0 +1,32 @@
+function solution(arr) {
+  let answer;
+  let max = 1; // 각 자리의 합이 같은 경우 비교 해주기 위한 변수
+
+  for (let x of arr) {
+    let temp = x; // while문 안에서 값을 가공하기 위한 변수
+    let sum = 0; // 각 자리의 합
+    while (temp) {
+      sum += temp % 10;
+      temp = Math.floor(temp / 10);
+
+      // if(temp === 0) { 0은 false 여서 이 부분이 필요 없다.
+      //   break;
+      // }
+    }
+    if (max < sum) {
+      max = sum;
+      answer = x;
+    } else if (sum === max) {
+      if (x > answer) {
+        max = sum;
+        answer = x;
+      }
+    }
+  }
+
+  return answer;
+}
+
+arr = [128, 460, 603, 40, 521, 137, 123];
+
+solution(arr);
