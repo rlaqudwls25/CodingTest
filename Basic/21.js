e5e9ee5560da83617f87a52b89956e8b8c725a9b
rlaqudwls25 <rlaqudwls157@gmail.com>
2022-04-05 14:48:15 +0900

    뒤집은 소수

---
+++ Basic/21.js
@@ -0,0 +1,21 @@
+function solution(arr) {
+  // 각 배열에 담겨 있는 숫자를 뒤집으려면 string => split => reverse => join()
+  // 각 숫자 소수 판별
+
+  let answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const num = parseInt(arr[i].toString().split("").reverse().join(""));
+    if (num % i !== 0 && num % i !== 1) {
+      answer.push(num);
+    }
+    {
+    }
+  }
+
+  return answer;
+}
+
+arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
+
+solution(arr);
