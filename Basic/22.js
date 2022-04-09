92a5a2897473cdee738b5ba791dc6a88c9dd9c9c
rlaqudwls25 <rlaqudwls157@gmail.com>
2022-04-09 14:23:04 +0900

    멘토링 풀이 추가

--- Basic/22.js
+++ Basic/22.js
@@ -2,31 +2,25 @@ function solution(arr) {
   const m = arr.length; // 행의 길이
   const n = arr[0].length; //학생의 길이
 
-  let count = 0;
   let answer = 0;
 
   for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= n; j++) {
-      console.log(i, j);
-
+      let count = 0;
       for (let k = 0; k < m; k++) {
+        //  행
         let p1 = 0;
         let p2 = 0;
         for (let z = 0; z < n; z++) {
-          if (arr[k][z] === i) {
-            p1 = z;
-          } else if (arr[k][z] === j) {
-            p2 = z;
-          }
+          // 열
+          if (arr[k][z] === i) p1 = z;
+          if (arr[k][z] === j) p2 = z;
         }
 
-        if (p1 < p2) {
-          count++;
-        }
+        if (p1 < p2) count++; // 0~3번 까지의 자리수로 등수를 판별 즉 0번째가 1등 3번째가 4등
       }
-      if (count === m) {
-        answer++;
-      }
+
+      if (count === m) answer++; // 모든 행의 만족을 해야하니 count가 3이어야 답이 나옴
     }
   }
 
