66232b1b5451619c5ede263d4529980ac64fbdb0
rlaqudwls25 <rlaqudwls157@gmail.com>
2022-07-05 00:38:01 +0900

    프로그래머스 1단계 없는 숫자 더하기 복습

--- Programmers/22.js
+++ Programmers/22.js
@@ -19,5 +19,20 @@ function solution(num) {
   return 45 - num.reduce((acc, cur) => acc + cur, 0);
 }
 
+// 세 번째 풀이
+function solution(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] === i) {
+        sum += arr[j];
+      }
+    }
+  }
+
+  return 45 - sum;
+}
+
 solution([1, 2, 3, 4, 6, 7, 8, 0]);
 solution([5, 8, 4, 0, 6, 7, 9]);
